99e9fd38b536b88992a36a94a9b4ff3de2873829
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-31 12:46:55 +0530

    CURD in mongoDB

--- server.js
+++ server.js
@@ -26,4 +26,37 @@ app.get('/todos', async (req, res) => {
   res.send(todos);
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.get('/todos/:id',async(req,res)=>{
+    const id = req.params.id;
+    const todo =await Todo.findOne({ id: id })
+    if(!todo){res.status(404).send("task not found")}
+    res.status(200).send(todo)
+})
+
+app.delete('/todo/delete/:id',async(req,res)=>{
+    const id =req.params.id
+    const todo = await Todo.deleteOne({id: id})
+    if(!todo){res.status(404).send("task not found")}
+    res.status(200).send( todo)
+})
+
+app.put('/todo/update/:id', async (req, res) => {
+  const id = req.params.id;
+  const updates = req.body; // JSON with fields to update
+
+  try {
+    const todo = await Todo.findOneAndUpdate(
+      { id: id },      
+      updates,         
+      { new: true }    
+    )
+
+    if (!todo) return res.status(404).send("Todo not found")
+
+    res.status(200).send(todo)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+app.listen(3000, () => console.log("Server running on port 3000"))
--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const todoSchema = new Schema({
+  id: {type: Number , required:true},
   title: { type: String, required: true },
   completed: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
