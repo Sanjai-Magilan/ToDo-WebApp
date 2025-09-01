508bec8028d8523bbb8cc78cd20c57af3950f961
Sanjai-Magilan <sanjai6115@gmail.com>
2025-09-01 08:23:52 +0530

    router implemented

--- server.js
+++ server.js
@@ -3,60 +3,9 @@ const connectDB = require("./db/database");
 const Todo = require("./models/todo"); // import schema
 require("dotenv").config();
 connectDB();
+const PORT= 5500
 const app = express();
 app.use(express.json());
-
-// DB connection
-
-// Routes
-app.post("/todos", async (req, res) => {
-  try {
-    const todo = new Todo(req.body);
-    await todo.save();
-    res.status(201).send(todo);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
-
-app.get("/todos", async (req, res) => {
-  const todos = await Todo.find();
-  res.send(todos);
-});
-
-app.get("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await Todo.findOne({ id: id });
-  if (!todo) {
-    res.status(404).send("task not found");
-  }
-  res.status(200).send(todo);
-});
-
-app.delete("/todo/delete/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await Todo.deleteOne({ id: id });
-  if (!todo) {
-    res.status(404).send("task not found");
-  }
-  res.status(200).send(todo);
-});
-
-app.put("/todo/update/:id", async (req, res) => {
-  const id = req.params.id;
-  const updates = req.body; // JSON with fields to update
-
-  try {
-    const todo = await Todo.findOneAndUpdate({ id: id }, updates, {
-      new: true,
-    });
-
-    if (!todo) return res.status(404).send("Todo not found");
-
-    res.status(200).send(todo);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-app.listen(3000, () => console.log("Server running on port 3000"));
+const route = require("./router/routes")
+app.use("/todo",route)
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
