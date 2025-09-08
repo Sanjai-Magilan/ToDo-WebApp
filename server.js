6f2bbc11094431a13a750418a89b08ba39621cc7
Sanjai-Magilan <sanjai6115@gmail.com>
2025-09-08 20:13:25 +0530

    ignored node modules

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const connectDB = require("./db/database");
+const route = require("./router/routes");
 connectDB();
 const PORT = 5500;
 const app = express();
 app.use(express.json());
-const route = require("./router/routes");
 app.use("/todo", route);
 console.log("NODE_ENV:", process.env.NODE_ENV);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
